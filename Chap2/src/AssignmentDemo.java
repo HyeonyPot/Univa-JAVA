b607b196d205e9b5da4a074bb51c8665c3a29f9e
PinkLemona <ryan999a@gmail.com>
2019-10-08 16:37:46 +0900

    Add files via upload

---
+++ Chap2/src/AssignmentDemo.java
@@ -0,0 +1,19 @@
+
+public class AssignmentDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value = 1;
+		System.out.println("값 = " + value);
+		value +=1;
+		System.out.println("값 = " + value);
+		value -=1;
+		System.out.println("값 = " + value);
+		value <<=3;
+		System.out.println("값 = " + value);
+		value %=3;
+		System.out.println("값 = " + value);
+		
+	}
+
+}
