527b3be2d8f6794e5d70b68d0e91f0c9cfd94fe0
PinkLemona <ryan999a@gmail.com>
2019-10-17 15:28:41 +0900

    재시도체크루프
    for문 이용한 재시도 체크루프

--- HW/Ngugudan.java
+++ HW/Ngugudan.java
@@ -1,11 +1,18 @@
+package HW;
 import java.util.Scanner;
 public class Ngugudan {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
+		Scanner ask = new Scanner(System.in);
 		int[] Ndan = new int[10];
 		int i;
+		String askq;
+		char q;
+		int on=1;
+		for(;on==1;){
+		
 		System.out.println("최대 10개의 구구단을 출력가능합니다.");
 		System.out.println("몇개의 구구단을 출력할까요?");
 		i=in.nextInt();
@@ -18,6 +25,16 @@ public class Ngugudan {
 		for(int m=0;m<i;m++)
 		gugudan(Ndan[m]);
 		
+		System.out.println("다시 입력하시겠습니까?(Y/N)");
+		askq = ask.nextLine();
+		q=askq.charAt(0);
+		if (q == 'y' || q == 'Y')
+		continue;
+		
+		else
+		on = 0;
+		}
+		System.out.println("구구단을 종료합니다.");
 	}
 	public static void gugudan(int dan){
 		System.out.println(dan+"단을 출력합니다.");
