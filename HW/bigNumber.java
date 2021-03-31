15224a7c11b5506f02a6e1ab171d018e2ed3285c
PinkLemona <ryan999a@gmail.com>
2021-03-31 15:45:00 +0900

    Update bigNumber.java

--- HW/bigNumber.java
+++ HW/bigNumber.java
@@ -1,36 +1,26 @@
 import java.util.Scanner;
-
 public class bigNumber {
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
-		System.out.println("3개의 정수를 입력하세요");
-		int b1 = scan.nextInt();
-		int b2 = scan.nextInt();
-		int b3 = scan.nextInt();
-		int big1;
-		int big2;
-		System.out.println(b1+"d"+b2+"d"+b3);
-		if(b1<=b2) {
-			big1=b2;
-			big2=b1;
-			if(b2<=b3) {
-				System.out.println(b3+b2+b1);
-			}		
-			else if(b1<b3) {
-				System.out.println(b3+b1+b1);
-			}
-		}
-		else if(b1<b3) {
-			big1=b3;
+		System.out.println("3개의 정수를 입력하세요.");
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		int temp;
+		if(a>b && b>c){
+			
+			
 			
 		}
-
-	
+		else if() {
+			
+		}
+		if() {
+			
+		}
 	}
-	
-	
 
 }
 
